d891df45cff412e0009f8b8c26e293b662da6866
rnawslgk <hamo35301@gmail.com>
2022-11-03 19:21:40 +0900

    setup

--- app/src/main/java/com/example/sudoku/MainActivity.java
+++ app/src/main/java/com/example/sudoku/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.sudoku;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TableLayout;
+import android.widget.TableRow;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TableLayout tableLayout = (TableLayout) findViewById(R.id.tableLayout);
+
+        TableRow tableRow1 = new TableRow(this);
+        tableLayout.addView(tableRow1);
+
+
     }
 }
