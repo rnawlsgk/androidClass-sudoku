72778be9914291305116aaa60c24809c7ad4fe36
rnawlsgk <hamo35301@gmail.com>
2022-11-04 14:48:03 +0900

    일단 화면 전체 채움

--- app/src/main/java/com/example/sudoku/MainActivity.java
+++ app/src/main/java/com/example/sudoku/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.sudoku;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.TableLayout;
@@ -16,9 +17,22 @@ public class MainActivity extends AppCompatActivity {
 
         TableLayout tableLayout = (TableLayout) findViewById(R.id.tableLayout);
 
-        TableRow tableRow1 = new TableRow(this);
-        tableLayout.addView(tableRow1);
+        Button buttons[][] = new Button[9][9];
 
+        TableRow.LayoutParams layoutParams = new TableRow.LayoutParams(TableRow.LayoutParams.WRAP_CONTENT, TableRow.LayoutParams.MATCH_PARENT, 1.0f);
+        TableLayout.LayoutParams tableLayoutLayoutParams = new TableLayout.LayoutParams(TableLayout.LayoutParams.WRAP_CONTENT, 0, 1.0f);
 
+        for (int i=0;i<9;i++)   {
+            TableRow tableRow = new TableRow(this);
+            tableRow.setBackgroundColor(Color.DKGRAY);
+
+            for (int j=0;j<9;j++){
+                buttons[i][j] = new Button(this);
+                buttons[i][j].setLayoutParams(layoutParams);
+                tableRow.addView(buttons[i][j]);
+            }
+            tableRow.setLayoutParams(tableLayoutLayoutParams);
+            tableLayout.addView(tableRow);
+        }
     }
 }
