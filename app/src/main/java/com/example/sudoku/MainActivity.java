477747879a9027d1783c5905a58951cd4eb213b9
rnawlsgk <hamo35301@gmail.com>
2022-11-23 09:02:46 +0900

    numberpad 동적생성 테스트안 백업

--- app/src/main/java/com/example/sudoku/MainActivity.java
+++ app/src/main/java/com/example/sudoku/MainActivity.java
@@ -10,6 +10,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.Window;
 import android.widget.Button;
+import android.widget.FrameLayout;
 import android.widget.LinearLayout;
 import android.widget.TableLayout;
 import android.widget.TableRow;
@@ -57,46 +58,7 @@ public class MainActivity extends AppCompatActivity {
         }
 
         //==================number Pad=====================
-        LinearLayout numberPadLinearLayout = (LinearLayout) findViewById(R.id.linearLayout);
-        LinearLayout.LayoutParams linearLayoutLayoutParams = new LinearLayout.LayoutParams(
-                50,
-                50
-        );
 
-        TextView numberPadTextView = new TextView(this);
-        numberPadTextView.setText("Input Number");
-
-//        TableLayout numberPadTableLayout = new TableLayout(this);
-//        numberPadTableLayout.setLayoutParams(linearLayoutLayoutParams);
-
-
-
-//        numberPadTableLayout.setGravity(Gravity.CENTER);
-//        numberPadTableLayout.setBackgroundColor(Color.BLACK);
-
-//        TableRow.LayoutParams numPadTableRowLayoutParams = new TableRow.LayoutParams(
-//                0,
-//                TableRow.LayoutParams.WRAP_CONTENT,
-//                1.0f
-//        );
-//
-//        for (int i=0;i<3;i++) {
-//            TableRow numberPadTableRow = new TableRow(this);
-//            for (int j=0;j<3;j++) {
-//                Button numberPadButtons = new Button(this);
-//                numberPadButtons.setText("1");
-//
-//                numberPadButtons.setLayoutParams(numPadTableRowLayoutParams);
-//                numberPadTableRow.addView(numberPadButtons);
-//            }
-//            numberPadTableRow.setLayoutParams(tableLayoutLayoutParams);
-//            numberPadTableLayout.addView(numberPadTableRow);
-//        }
-//
-//        numberPadLinearLayout.addView(numberPadTextView);
-//        numberPadLinearLayout.addView(numberPadTableLayout);
-//
-//        numberPadTableLayout.setVisibility(View.INVISIBLE);
 
     }
 
